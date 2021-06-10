fa231c804600fef568d31767e546c838255bf84f
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2021-06-10 07:36:31 +0300

    my portfolio

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -23,7 +23,7 @@ import { Button, Card, Container, Row, Col } from "reactstrap";
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import profile from "assets/img/theme/profile2.jfif";
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -124,20 +124,20 @@ class Profile extends React.Component {
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Roa'a Abu Aligah{" "}
+                      <span className="font-weight-light">, 25</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Irbid, Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Instructor - coding program
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      LTUC - ASAC
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
